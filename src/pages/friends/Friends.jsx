acbd7f715e98ae33e5ef3ea72de2e8f2655aad61
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-26 19:55:13 +0530

    Update Friends.jsx

--- src/pages/friends/Friends.jsx
+++ src/pages/friends/Friends.jsx
@@ -1,1 +1,18 @@
 
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Follower from "../../components/follower/Follower";
+import Following from "../../components/following/Following";
+import Terms from "../Terms/Terms";
+
+const Friends = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Following />} />
+      <Route path="/follower" element={<Follower />} />
+      <Route path="/terms" element={<Terms />} />
+    </Routes>
+  );
+};
+
+export default Friends;
