4f9163a783994de2fbe8bc8532b08723df7602a6
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-26 16:14:22 +0530

    Update Sports.jsx

--- src/components/sports/Sports.jsx
+++ src/components/sports/Sports.jsx
@@ -10,48 +10,30 @@ const Sports = () => {
   useEffect(() => {
     getData();
   }, []);
-//   let getData = async () => {
-//     let res = await fetch('https://api.currentsapi.services/v1/search?' +
-//             'keywords=sports&language=en&' + 
-//             'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0');
-//     let data = await res.json();
-//     console.log(data);
-//     setNews(data.news);
-//   };
-//     let getData=async()=>{
-//   const proxyUrl = "https://crowdhub.netlify.app/"
-// const qInTitle = "sports";
-// const apiKey = "sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0";
-// const url = `${proxyUrl}https://api.currentsapi.services/v1/search?keywords=${qInTitle}&language=en&apiKey=${apiKey}`;
-// const request = new Request(url);
-// fetch(request)
-//   .then(response =>{
-//         console.log(response);
-//         response.json()})
-//   .then((data) => {
-//     console.log(data);
-//    setNews(data.news);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-//     }
+
     let getData=()=>{
-    return axios('https://api.currentsapi.services/v1/search?' +
-            'keywords=sports&language=en&' + 
-            'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0', {
-      method: 'GET'||'POST',
-       headers: {
-        'Access-Control-Allow-Origin': 'https://crowdhub.netlify.app',
-        'Content-Type': 'application/json',
-      },
-      mode: 'cors',
-      withCredentials: true,
-    }).then(response => {
-      let data= response.json();
-      console.log(data)
-      setNews(data.news);
+//     return axios('https://api.currentsapi.services/v1/search?' +
+//             'keywords=sports&language=en&' + 
+//             'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0', {
+//       method: 'GET'||'POST',
+//        headers: {
+//         'Access-Control-Allow-Origin': 'https://crowdhub.netlify.app',
+//         'Content-Type': 'application/json',
+//       },
+//       mode: 'cors',
+//       withCredentials: true,
+//     }).then(response => {
+//       let data= response.json();
+//       console.log(data)
+//       setNews(data.news);
+//     })
+      info="sports";
+       const { isLoading, error, data } = useQuery(["users", info], () =>
+    makeRequest.get("users/news?searchId=" + info).then((res) => {
+      return res.data;
     })
+  );
+      
     }
   return (
     <div className="news">
