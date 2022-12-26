b765ee6b90c7e5f003551e6aebc824410afe3a9c
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-26 14:43:02 +0530

    Update Sports.jsx

--- src/components/sports/Sports.jsx
+++ src/components/sports/Sports.jsx
@@ -10,32 +10,32 @@ const Sports = () => {
   useEffect(() => {
     getData();
   }, []);
-//   let getData = async () => {
-//     let res = await fetch('https://api.currentsapi.services/v1/search?' +
-//             'keywords=sports&language=en&' + 
-//             'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0');
-//     let data = await res.json();
-//     console.log(data);
-//     setNews(data.news);
-//   };
-    let getData=()=>{
-    return axios('https://api.currentsapi.services/v1/search?' +
+  let getData = async () => {
+    let res = await fetch('https://api.currentsapi.services/v1/search?' +
             'keywords=sports&language=en&' + 
-            'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0', {
-      method: 'GET',
-       headers: {
-        'Access-Control-Allow-Origin': 'https://crowdhub.netlify.app',
-        'Content-Type': 'application/json',
-      },
-      mode: 'no-cors',
-      withCredentials: true,
-      credentials: 'same-origin',
-    }).then(response => {
-      let data= response.json();
-      console.log(data)
-      setNews(data.news);
-    })
-    }
+            'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0');
+    let data = await res.json();
+    console.log(data);
+    setNews(data.news);
+  };
+//     let getData=()=>{
+//     return axios('https://api.currentsapi.services/v1/search?' +
+//             'keywords=sports&language=en&' + 
+//             'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0', {
+//       method: 'GET',
+//        headers: {
+//         'Access-Control-Allow-Origin': 'https://crowdhub.netlify.app',
+//         'Content-Type': 'application/json',
+//       },
+//       mode: 'no-cors',
+//       withCredentials: true,
+//       credentials: 'same-origin',
+//     }).then(response => {
+//       let data= response.json();
+//       console.log(data)
+//       setNews(data.news);
+//     })
+//     }
   return (
     <div className="news">
       <h2>All about Sports</h2>
