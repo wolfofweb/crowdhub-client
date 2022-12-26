1f2400f1eac9c8f89f3d20bcc9f5e4b2a1da2d32
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-26 13:43:06 +0530

    Update Sports.jsx

--- src/components/sports/Sports.jsx
+++ src/components/sports/Sports.jsx
@@ -18,55 +18,6 @@ const Sports = () => {
     console.log(data);
     setNews(data.news);
   };
-//    let getData=()=>{
-//     return axios( `https://newsapi.org/v2/everything?q=sports&sortBy=popularity?country=in&apiKey=a54a083a384e4e2fa8a09733da5a3c38`, {
-//       method: 'GET',
-//       mode: 'no-cors',
-//       headers: {
-//         'Access-Control-Allow-Origin': 'https://crowdhub.netlify.app',
-//         'Content-Type': 'application/json',
-//       },
-//       withCredentials: 'omit',
-//       credentials: 'same-origin',
-//     }).then(response => {
-//       let data= response.json();
-//       console.log(data)
-//       setNews(data.articles);
-//     })
-
-//   }
-//   let getData=()=>{
-//   const proxyUrl = "https://crowdhub.netlify.app/"
-// const qInTitle = "sports";
-// const apiKey = "a54a083a384e4e2fa8a09733da5a3c38";
-// const url = `${proxyUrl}https://newsapi.org/v2/everything?qInTitle=${qInTitle}&sortBy=popularity?country=in&apiKey=${apiKey}`;
-// const request = new Request(url);
-
-// // fetch(request)
-// //   .then(response => response.json())
-// //   .then((data) => {
-// //     console.log(data);
-// //    setNews(data.articles);
-// //   })
-// //   .catch(error => {
-// //     console.log(error);
-// //   });
-//      let res = await fetch(request);
-//     let data = await res.json();
-//     console.log(data);
-//     setNews(data.articles);
-//      var url = 'https://api.currentsapi.services/v1/search?' +
-//             'keywords=sports&language=en&' + 
-//             'apiKey=sRm4uU7AOS5nIyJilHwoxsoKCkbbDRsfRMI2KZWLwqbjAVK0';
-//     var req = new Request(url);
-//     fetch(req).then(response => {
-//       let data= response.json();
-//       console.log(data)
-//       setNews(data.news);
-//     })
-       
-//   }
-  
   
   return (
     <div className="news">
